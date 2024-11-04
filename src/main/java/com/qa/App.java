44b30ced0c117340410c5ac26e087faf634c9380
danielleball <daniellejb01@hotmail.com>
2024-11-04 13:33:00 +0000

    added 'Danielle'

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHellotoSomeone("Danielle"));
         System.out.println(sayGoodbye());
     }
 
